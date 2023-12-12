595e2d0961a73b9053fc06d3fe003188fc42231b
AlexanderK <kriss.sacha@gmail.com>
2023-12-12 16:44:51 +0300

    fix

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,15 +1,14 @@
 import {Row} from "react-bootstrap";
 import React from "react";
 import data from "../data/data";
+import Container from "./utils/Container";
 
 
 export default function Title() {
-    return <div className="max-width">
-        <div className="pad">
-            <Row className="justify-content-center">
-        <div className="text-center main-title">{data.header.title}</div>
-        <div className="text-center sub-title">{data.header.subtitle}</div>
-    </Row>
-        </div>
-    </div>
+    return <Container>
+        <Row className="justify-content-center">
+            <div className="text-center main-title">{data.header.title}</div>
+            <div className="text-center sub-title">{data.header.subtitle}</div>
+        </Row>
+    </Container>
 }
