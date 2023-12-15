414d64936d8da167811dea7bab043dd948418a84
AlexanderK <kriss.sacha@gmail.com>
2023-12-15 18:26:08 +0300

    fix

--- src/components/teem/Card.js
+++ src/components/teem/Card.js
@@ -1,24 +1,27 @@
 import {Col, Image, Row} from "react-bootstrap";
-import React from "react";
 import './teem.css'
+import React from "react";
 
 
-export default function Card({participant}) {
+export default function Card({participant, empty=""}) {
+
+
+
     return <div className="card-player">
         <Row>
-            <a href={participant.statistics[0].href}><Image src={participant.photo} className="img-size"></Image></a>
+            <a className={`${empty}`} href={participant.statistics[0].href}><Image src={participant.photo} className="img-size"></Image></a>
         </Row>
         <div className="tab pt-3">
-            <div className="name">{participant.name}</div>
+            <div className={`name ${empty}`}>{participant.name}</div>
             <Row xs="auto" className="icons pt-2">
                 {participant.statistics.map(({icon, href, audience, id}) => (
                     <Col key={id}>
                         <Row xs="auto" className="align-items-center p-2">
-                            <a href={href} className="noStyle p-0" style={{height: "fit-content"}}>
+                            <a href={href} className={`noStyle p-0 ${empty}`} style={{height: "fit-content"}}>
                                 <Image className="icon-global" src={icon}/>
                             </a>
-                            <a href={href} className="noStyle p-0 ps-1">
-                                <div className="stats-card">{audience}</div>
+                            <a href={href} className={`noStyle p-0 ps-1 ${empty}`}>
+                                <div className={`stats-card ${empty}`}>{audience}</div>
                             </a>
                         </Row>
                     </Col>))}
--- src/components/teem/Teem.js
+++ src/components/teem/Teem.js
@@ -1,27 +1,64 @@
 import {Row} from "react-bootstrap";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Card from "./Card";
 import data from "../../data/data";
-import { CCarousel, CCarouselItem} from '@coreui/react'
+import {CCarousel, CCarouselItem} from '@coreui/react'
 import '@coreui/coreui/dist/css/coreui.min.css'
 import Container from "../utils/Container";
+import emptyPhoto from '../../data/img/empty.png'
+import emptyIcon from '../../data/icons/empty.svg'
+
+function sliceIntoChunks(arr, chunkSize) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+        let chunk = arr.slice(i, i + chunkSize);
+        res.push(chunk.map(p => <Card key={p.name} participant={p}/>));
+    }
+
+    const last = chunkSize - (arr.length % chunkSize == 0? chunkSize : arr.length % chunkSize)
+    for (let i = 0; i < last; i++) {
+        res[res.length-1].push(<Card key={i} empty={"empty"} participant={{
+            name: ".",
+            photo: emptyPhoto,
+            statistics: [
+                {
+                    href: "",
+                    icon: emptyIcon,
+                    audience: ".",
+                    id: 0
+                }
+            ]
+        }}/>)
+    }
+    return res;
+}
 
 export default function Teem() {
+    const [teem, setTeem] = useState(sliceIntoChunks(data.team.participants, 6))
+    const [isSmall, setIsSmall] = useState(window.innerWidth < 1150)
+    const handleResize = (e) => {
+        setIsSmall(e.target.innerWidth < 1150)
+    }
+    useEffect(() => {
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
+    useEffect(() => {
+        if (isSmall)
+            setTeem(sliceIntoChunks(data.team.participants, 4))
+        else
+            setTeem(sliceIntoChunks(data.team.participants, 6))
+    }, [isSmall]);
+
     return <CCarousel controls indicators className="mb-5 gabs">
-        <CCarouselItem>
+        {teem.map((par, index) =>  <CCarouselItem key={index}>
             <Container>
                 <Row className="justify-content-around pad-teem">
-                    {data.team.participants.map(p => <Card key={p.name} participant={p}/>)}
+                    {par}
                 </Row>
             </Container>
-        </CCarouselItem>
-        <CCarouselItem>
-            <Container>
-                <Row className="justify-content-around pad-teem">
-                    {data.team.participants.map(p => <Card key={p.name} participant={p}/>)}
-                </Row>
-            </Container>
-        </CCarouselItem>
+        </CCarouselItem>)}
     </CCarousel>
 
 }
